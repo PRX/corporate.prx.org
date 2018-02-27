26deb319b25f4b9b1a837ea12704c55b1488ac45
gcampo88 <gigi.campo@gmail.com>
2018-02-27 17:24:54 -0500

    Affix Company-About sidebar on vertical scroll

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,25 @@ function loadAllImages() {
   }
 }
 
+function affixCompanyNav(e) {
+  var companyAboutNav = document.getElementsByClassName('fixed-nav-container')[0];
+  var scrollY = window.scrollY;
+
+  if (companyAboutNav) {
+    if (scrollY > 100) {
+      // TODO this is a little jumpy
+      companyAboutNav.className = 'fixed-nav-container no-scroll';
+    } else {
+      companyAboutNav.className = 'fixed-nav-container';
+    }
+  }
+}
+
 // The event subscription that loads images when the page is ready
 document.addEventListener('DOMContentLoaded', loadAllImages);
 
 // The event subscription that reloads images on resize
 window.addEventListener('resize', loadAllImages);
+
+// On vertical scrolling, we want to fix the About-Company side navigation in place
+window.addEventListener('scroll', affixCompanyNav)
