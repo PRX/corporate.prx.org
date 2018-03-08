1d772301928375b726708f0edb1503cbe26a93fe
gcampo88 <gigi.campo@gmail.com>
2018-03-08 16:13:48 -0500

    Refactor company page-related js

--- scripts/activate-links.js
+++ scripts/activate-links.js
@@ -1,17 +1,6 @@
 // event listeners for the Company pages and Company - About page, to apply "active" classes
 // to nav links and to affix the About page side nav in place on scroll
 
-function affixCompanyNav(e) {
-  var companyAboutNav = document.getElementsByClassName('fixed-nav-container')[0];
-  if (companyAboutNav) {
-    if (window.scrollY > 140) {
-      companyAboutNav.className = 'fixed-nav-container no-scroll';
-    } else {
-      companyAboutNav.className = 'fixed-nav-container';
-    }
-  }
-}
-
 function activateCompanyLinkInMainNav(e) {
   var onCompanyPages = !!window.location.href.match(/company/);
   if (onCompanyPages) {
@@ -29,29 +18,38 @@ function isElementInViewport (el) {
     );
 }
 
-function activateAboutCompanyLink(e) {
+function affixNav(sideNav) {
+  if (window.scrollY > 140) {
+    sideNav.className = 'fixed-nav-container no-scroll';
+  } else {
+    sideNav.className = 'fixed-nav-container';
+  }
+}
 
+function removeActiveClassFromAll(elements) {
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].className = elements[i].className.replace(/active/, '');
+  }
+}
+
+function affixAndStyleAboutCompanyNav(e) {
   var onCompanyAbout = !!window.location.href.match(/company\/about/);
   if (onCompanyAbout) {
+    var companyAboutNav = document.getElementsByClassName('fixed-nav-container')[0];
+    affixNav(companyAboutNav)
+
+    var contentLinks = companyAboutNav.children;
+    removeActiveClassFromAll(contentLinks);
 
     var contentSections = document.getElementsByClassName('prx-about-company-content')[0].children;
-    var contentLinks = document.getElementsByClassName('fixed-nav-container')[0].children;
 
-    // remove active styling for every link in side nav
-    for (var i = 0; i < contentLinks.length; i++) {
-      contentLinks[i].className = contentLinks[i].className.replace(/active/, '');
-    }
-
-    // find the first section of page that has top in focus
     for (var i = 0; i < contentSections.length; i++) {
       if (isElementInViewport(contentSections[i])) {
-
         // ordering is consistent between sections & side nav, so just use same index
         var activeLink = contentLinks[i];
         if (activeLink && !activeLink.className.match(/active/)) {
           activeLink.className = activeLink.className + ' active';
         }
-
         // don't add active styles to others that might be on page
         break;
       }
@@ -75,8 +73,5 @@ try {
   passiveSupported = false;
 }
 
-window.addEventListener('scroll', affixCompanyNav, passiveSupported ? { passive: true } : false);
-
 window.addEventListener('DOMContentLoaded', activateCompanyLinkInMainNav, passiveSupported ? { passive: true } : false);
-
-window.addEventListener('scroll', activateAboutCompanyLink, passiveSupported ? { passive: true } : false);
+window.addEventListener('scroll', affixAndStyleAboutCompanyNav, passiveSupported ? { passive: true } : false);
