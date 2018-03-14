7f6c4a966543207dc12c981a162ffdac186edf23
gcampo88 <gigi.campo@gmail.com>
2018-03-14 14:22:09 -0400

    Add click listener to hamburger menu to show Company navigation on mobile

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,27 @@ function loadAllImages() {
   }
 }
 
+function addHamburgerClickListener(e) {
+  document.getElementById("prx-main-nav-menu-button").addEventListener("click", function(){
+    showCompanyNav();
+  });
+}
+
+function showCompanyNav() {
+  var currentlyCheckingShowMenu = document.getElementById("prx-show-menu").checked === false;
+  var companyNavParentEl = document.getElementById("prx-company-nav").parentElement;
+  if (currentlyCheckingShowMenu) {
+    companyNavParentEl.className = companyNavParentEl.className + " revealed";
+  } else {
+    companyNavParentEl.className = companyNavParentEl.className.replace(/revealed/, '');
+  }
+}
+
 // The event subscription that loads images when the page is ready
 document.addEventListener('DOMContentLoaded', loadAllImages);
 
 // The event subscription that reloads images on resize
 window.addEventListener('resize', loadAllImages);
+
+// add click listener for hamburger menu button
+document.addEventListener('DOMContentLoaded', addHamburgerClickListener);
