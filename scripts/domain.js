6b4c222d82073aca76d2535d13bcee77505205eb
Chris Kalafarski <chris@farski.com>
2018-03-20 16:56:47 -0400

    Redirect non-canonical traffic

---
+++ scripts/domain.js
@@ -0,0 +1,5 @@
+// if (!window.location.href.includes('https://www.prx.org/') &&
+//     !window.location.href.includes('squarespace.com/config')) {
+//   window.location.replace('https://www.prx.org/');
+// }
+
