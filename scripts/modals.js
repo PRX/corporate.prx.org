f541a4fb4db47d52a84d953bb149ca5e4f18ed45
Chris Kalafarski <chris@farski.com>
2018-03-28 10:35:18 -0400

    Add support for targetting modals from href's

--- scripts/modals.js
+++ scripts/modals.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   const modals = document.querySelectorAll('.prx-modal-wrapper');
 
+  // Add some behavior to the modals themselves
   modals.forEach(function (modal) {
     const buttons = modal.querySelectorAll('.prx-modal-dismiss-button')
 
@@ -16,6 +17,20 @@ document.addEventListener('DOMContentLoaded', function () {
           break;
         }
       });
+    });
+  });
+
+  // There are often cases where a button that is managed through the CMS needs
+  // to trigger a modal. Those buttons can be tough to target, since there's no
+  // way to predict or define the ID/class/etc. Instead we use the href, and
+  // build specially-constructed anchors, that can trigger modals by name.
+  // eg, href="#prx-modal_prx-exchange-donate-form"
+  document.querySelectorAll('a[href^="#prx-modal_"]').forEach(function (link) {
+    link.addEventListener('click', function (ev) {
+      ev.preventDefault();
+
+      const modalId = link.getAttribute('href').replace('#prx-modal_', '');
+      document.getElementById(modalId).classList.add('active');
     });
   });
 });
