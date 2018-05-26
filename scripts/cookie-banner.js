6be04d9c7fcda0e85647c907b467e52f0dde0760
Andrew Kuklewicz <andrew@beginsinwonder.com>
2018-05-26 09:14:44 -0400

    Change all quotes to single quotes

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -1,19 +1,12 @@
-// <script
-//   src="https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js"
-//   type="text/javascript"
-//   id="cookiebanner"
-//   data-position="top"
-//   data-moreinfo="https://www.prx.org/privacy-policy"
-// ></script>
 document.addEventListener('DOMContentLoaded', function () {
   const src = 'https://cdnjs.cloudflare.com/ajax/libs/cookie-banner/1.2.2/cookiebanner.min.js';
 
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = src;
-  script.id = "cookiebanner";
-  script.setAttribute("data-position", "top");
-  script.setAttribute("data-moreinfo", "https://www.prx.org/privacy-policy");
+  script.id = 'cookiebanner';
+  script.setAttribute('data-position', 'top');
+  script.setAttribute('data-moreinfo', 'https://www.prx.org/privacy-policy');
 
   document.getElementsByTagName('head')[0].appendChild(script);
 });
