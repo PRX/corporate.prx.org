5d67c66f4af2fcfba328e851c35b574ce9d23157
Chris Kalafarski <chris@farski.com>
2021-02-25 14:41:10 -0500

    Station grid styles

--- scripts/station-homepage.js
+++ scripts/station-homepage.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const grid = document.getElementById('prx-station-program-grid');
     const tiles = grid.getElementsByTagName('li');
 
-    const fillerCount = (6 - (tiles.length % 6));
+    const fillerCount = (4 - (tiles.length % 4));
 
     for (let index = 0; index < fillerCount; index++) {
       const li = document.createElement('li');
